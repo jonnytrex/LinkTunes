e5dabb1525e6b45c38ef713cd55bfa1d990e05a7
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 <trexlernoah@gmail.com>
2023-11-14 21:29:16 -0500

    angular bootstrap template

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeComponent, ProfileComponent],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule,
+    HomeComponent,
+    ProfileComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbDatepicker, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbDatepicker],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private modalService: NgbModal) {}
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+}
