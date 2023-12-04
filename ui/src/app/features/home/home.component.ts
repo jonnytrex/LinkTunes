959116e13e587a44b7015824004f5cd06d901c72
Noah Trexler <trexlernoah@gmail.com>
2023-12-03 21:28:01 -0500

    music player

--- ui/src/app/features/home/home.component.ts
+++ ui/src/app/features/home/home.component.ts
@@ -1,12 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgbDatepicker, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgxAudioPlayerModule, Track } from 'ngx-audio-player';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NgbDatepicker],
+  imports: [CommonModule, NgbDatepicker, NgxAudioPlayerModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  mssapDisplayTitle = true;
+  mssapDisablePositionSlider = true;
+  mssapDisplayRepeatControls = true;
+  mssapDisplayVolumeControls = true;
+  mssapDisplayVolumeSlider = false;
+
+  // Material Style Simple Audio Player
+  mssapPlaylist: Track[] = [
+    {
+      title: 'Strange',
+      link: '../../../assets/Strange.mp3',
+      artist: 'Galaxie 500',
+      duration: 180,
+    },
+  ];
+}
